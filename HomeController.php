f2f2f8d1dc6f0f5b461402359f8d2070ad3981e2
Koussaybenhadjsalem <100635973+Koussaybenhadjsalem@users.noreply.github.com>
2023-10-01 20:33:08 +0100

    Add files via upload

---
+++ HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/show', name: 'app_show')]
+    public function show(): Response
+    {
+        return new Response('<h1>bonjour 3a39</h1>');
+    }
+    #[Route('/msg', name: 'msg')]
+    public function showmsg(): Response
+    {
+        return $this->render('home/msg.html.twig');
+    }
+    
+    
+  
+  
+    
+    
+}
